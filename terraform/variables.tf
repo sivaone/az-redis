99625c7d059bfdfaa02a4604641e857d7cf74221
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2023-05-09 11:34:54 -0500

    Create resource group

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "rg_name" {
+  type        = string
+  description = "Resource Group Name"
+}
+
+variable "location" {
+  type        = string
+  description = "Resource location"
+  default     = "eastus2"
+}
