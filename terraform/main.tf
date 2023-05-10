883bfc3e754f7a0d42d057fa71cdad5521a5f285
Siva Nagi Reddy Alla <siva.alla@outlook.com>
2023-05-10 18:35:01 -0500

    Add service and ingress controller in az

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,29 @@ resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.location
 }
+
+resource "azurerm_kubernetes_cluster" "kube" {
+  name                = "svkube"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "svkube"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = var.node_vm_size
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Env = "Kube"
+  }
+}
+
+output "kube_config" {
+  value     = azurerm_kubernetes_cluster.kube.kube_config_raw
+  sensitive = true
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,9 @@ variable "location" {
   description = "Resource location"
   default     = "eastus2"
 }
+
+variable "node_vm_size" {
+  type        = string
+  description = "Node pool vm size"
+  default     = "Standard_D2_v2"
+}
